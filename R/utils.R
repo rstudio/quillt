931b98ddcfc822eda6c519043b70f230a1a9f18c
christophe dervieux <christophe.dervieux@gmail.com>
2021-02-26 15:37:17 +0100

    Add a function to check installed package

---
+++ R/utils.R
@@ -0,0 +1,8 @@
+check_install <- function(pkgs) {
+    inst <- vapply(pkgs, requireNamespace, quietly = TRUE, FUN.VALUE = logical(1))
+    if (all(inst)) {
+      return()
+    }
+    stop("Must install the following packages to use this function:\n",
+         paste0("* ", pkgs[!inst], "\n"), call. = FALSE)
+}
