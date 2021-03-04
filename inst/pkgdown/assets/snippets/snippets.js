e537cd060e3875cc7260366291b0a6c6c14837d7
Garrick Aden-Buie <garrick@adenbuie.com>
2021-03-04 10:32:26 -0500

    Add layout.html and use after-body-reference-topic.html

--- inst/pkgdown/assets/snippets/snippets.js
+++ inst/pkgdown/assets/snippets/snippets.js
@@ -21,32 +21,3 @@ function loadSnippet(snippet, mode) {
     });
   });
 }
-
-function relocateFunctions() {
-  if (!document.querySelector('.template-reference-topic')) {
-    return;
-  }
-
-  // grab functions and next element, the list of functions
-  var fns = document.getElementById('functions');
-  if (!fns) { return; }
-  fns = [fns, fns.nextElementSibling];
-
-  // locate usage section
-  var usage = document.getElementById('usage');
-  if (!usage) { return; }
-
-  // Move Functions before Usage
-  fns.forEach(function(el) {
-    usage.parentElement.insertBefore(el, usage);
-  });
-
-  // Redraw Toc
-  if (typeof window.Toc !== "undefined") {
-    document.getElementById('toc').removeChild(document.querySelector('#toc .nav'))
-    $('nav[data-toggle="toc"]').each(function(i, el) {
-      var $nav = $(el);
-      Toc.init($nav);
-    });
-  }
-}
